e38a0839b3a3ee6954cabd99295850c044c4ead9
upwork0228 <amya84392@gmail.com>
2024-05-17 02:07:00 -0400

    ai-voice-assist-dora

---
+++ Dora AI/Dora AI.py
@@ -0,0 +1,291 @@
+import subprocess
+import wolframalpha
+import pyttsx3
+import tkinter
+import json
+import random
+import operator
+import speech_recognition as sr
+import datetime
+import wikipedia
+import webbrowser
+import os
+import winshell
+import pyjokes
+import feedparser
+import smtplib
+import ctypes
+import time
+import requests
+import shutil
+from twilio.rest import Client
+from clint.textui import progress
+from ecapture import ecapture as ec
+from bs4 import BeautifulSoup
+import win32com.client as wincl
+from urllib.request import urlopen
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+def speak(audio):
+	engine.say(audio)
+	engine.runAndWait()
+
+def wishMe():
+	hour = int(datetime.datetime.now().hour)
+	if hour>= 0 and hour<12:
+		speak("Good Morning Sir !")
+
+	elif hour>= 12 and hour<18:
+		speak("Good Afternoon Sir !") 
+
+	else:
+		speak("Good Evening Sir !") 
+
+	assname =("Dora AI")
+	speak("I am your Assistant")
+	speak(assname)
+	
+
+def username():
+	speak("What should i call you sir")
+	uname = takeCommand()
+	speak("Welcome Mister") 
+	speak(uname)
+	columns = shutil.get_terminal_size().columns
+	
+	print("#####################".center(columns))
+	print("Welcome Mr.", uname.center(columns))
+	print("#####################".center(columns))
+	
+	speak("How can i Help you Sir?")
+
+def takeCommand():
+	
+	r = sr.Recognizer()
+	
+	with sr.Microphone() as source:
+		
+		print("Listening...")
+		r.pause_threshold = 1
+		audio = r.listen(source)
+
+	try:
+		print("Recognizing...") 
+		query = r.recognize_google(audio, language ='en-in')
+		print(f"User said: {query}\n")
+
+	except Exception as e:
+		print(e) 
+		print("Unable to Recognize your voice.") 
+		return "None"
+	
+	return query
+
+def sendEmail(to, content):
+	server = smtplib.SMTP('2k22.csai.2213050@gmail.com', 'Rida012@')
+	server.ehlo()
+	server.starttls()
+	
+	# Enable low security in gmail
+	server.login('2k22.csai.2213313@gmail.com', '@Himanshu1234')
+	server.sendmail('2k22.csai.2213313@gmail.com', to, content)
+	server.close()
+
+if __name__ == '__main__':
+	clear = lambda: os.system('cls')
+	
+	# This Function will clean any
+	# command before execution of this python file
+	clear()
+	wishMe()
+	username()
+	
+	while True:
+		
+		query = takeCommand().lower()
+		
+		# All the commands said by user will be 
+		# stored here in 'query' and will be
+		# converted to lower case for easily 
+		# recognition of command
+		if 'open linkedin' in query:
+			speak("Here you go to Linkedin\n")
+			webbrowser.open("https://www.linkedin.com/?original_referer=")	
+
+		elif 'open youtube' in query:
+			speak("Here you go to Youtube\n")
+			webbrowser.open("youtube.com")
+
+		elif 'open google' in query:
+			speak("Here you go to Google\n")
+			webbrowser.open("google.com")
+
+		elif 'how r you' in query:
+			speak("I am fine, Thank you")
+			speak("How are you, Sir")
+
+		elif 'fine' in query or "good" in query:
+			speak("It's good to know that your fine")
+
+		elif "change name" in query:
+			speak("What would you like to call me, Sir ")
+			assname = takeCommand()
+			speak("Thanks for naming me")
+
+		elif 'exit' in query:
+			speak("Thanks for giving me your time")
+			exit()
+
+		elif "who made you" in query or "who created you" in query: 
+			speak("I have been created by Himanshu.")
+			
+		elif 'joke' in query:
+			speak(pyjokes.get_joke())
+
+
+		elif "who i am" in query:
+			speak("If you talk then definitely your human.")
+
+		elif "why you came to world" in query:
+			speak("Thanks to Himanshu. further It's a secret")
+
+		elif 'power point presentation' in query:
+			speak("opening Power Point presentation")
+			power = r"C:\\Users\\GAURAV\\Desktop\\Minor Project\\Presentation\\Voice Assistant.pptx"
+			os.startfile(power)
+
+		elif 'What is love' in query:
+			speak("It is 7th sense that destroy all other senses")
+
+		elif 'reason for you' in query:
+			speak("I was created as a Mini project by Mister Himanshu ")
+
+
+		elif 'news' in query:
+			
+			try: 
+				jsonObj = urlopen('''https://newsapi.org / v1 / articles?source = the-times-of-india&sortBy = top&apiKey =\\times of India Api key\\''')
+				data = json.load(jsonObj)
+				i = 1
+				
+				speak('here are some top news from the times of india')
+				print('''=============== TIMES OF INDIA ============'''+ '\n')
+				
+				for item in data['articles']:
+					
+					print(str(i) + '. ' + item['title'] + '\n')
+					print(item['description'] + '\n')
+					speak(str(i) + '. ' + item['title'] + '\n')
+					i += 1
+			except Exception as e:
+				
+				print(str(e))
+
+		
+		elif 'lock window' in query:
+				speak("locking the device")
+				ctypes.windll.user32.LockWorkStation()
+
+		elif 'shutdown system' in query:
+				speak("Hold On a Sec ! Your system is on its way to shut down")
+				subprocess.call('shutdown / p /f')
+				
+		elif 'empty recycle bin' in query:
+			winshell.recycle_bin().empty(confirm = False, show_progress = False, sound = True)
+			speak("Recycle Bin Recycled")
+
+		elif "don't listen" in query or "stop listening" in query:
+			speak("for how much time you want to stop dhokebaaz from listening commands")
+			a = int(takeCommand())
+			time.sleep(a)
+			print(a)
+
+		elif "where is" in query:
+			query = query.replace("where is", "")
+			location = query
+			speak("User asked to Locate")
+			speak(location)
+			webbrowser.open("https://www.google.nl / maps / place/" + location + "")
+
+		elif "camera" in query or "take a photo" in query:
+			ec.capture(0, " Camera ", "img.jpg")
+
+		elif "restart" in query:
+			subprocess.call(["shutdown", "/r"])
+			
+		elif "hibernate" in query or "sleep" in query:
+			speak("Hibernating")
+			subprocess.call("shutdown / h")
+
+		elif "log off" in query or "sign out" in query:
+			speak("Make sure all the application are closed before sign-out")
+			time.sleep(5)
+			subprocess.call(["shutdown", "/l"])
+
+		elif "write a note" in query:
+			speak("What should i write, sir")
+			note = takeCommand()
+			file = open('jarvis.txt', 'w')
+			speak("Sir, Should i include date and time")
+			snfm = takeCommand()
+			if 'yes' in snfm or 'sure' in snfm:
+				strTime = datetime.datetime.now().strftime("% H:% M:% S")
+				file.write(strTime)
+				file.write(" :- ")
+				file.write(note)
+			else:
+				file.write(note)
+		
+		elif "show note" in query:
+			speak("Showing Notes")
+			file = open("jarvis.txt", "r") 
+			print(file.read())
+			speak(file.read(6))
+
+		elif "update assistant" in query:
+			speak("After downloading file please replace this file with the downloaded one")
+			url = '# url after uploading file'
+			r = requests.get(url, stream = True)
+			
+			with open("Voice.py", "wb") as Pypdf:
+				
+				total_length = int(r.headers.get('content-length'))
+				
+				for ch in progress.bar(r.iter_content(chunk_size = 2391975),
+									expected_size =(total_length / 1024) + 1):
+					if ch:
+						Pypdf.write(ch)
+					
+		# NPPR9-FWDCX-D2C8J-H872K-2YT43
+
+		elif "Good Morning" in query:
+			speak("A warm" +query)
+			speak("How are you Mister")
+			speak(assname)
+
+		# most asked question from google Assistant
+		elif "will you be my gf" in query or "will you be my bf" in query: 
+			speak("I'm not sure about, may be you should give me some time")
+
+		elif "i love you" in query:
+			speak("It's hard to understand")
+
+		elif "what is" in query or "who is" in query:
+			
+			# Use the same API key 
+			# that we have generated earlier
+			client = wolframalpha.Client("API_ID")
+			res = client.query(query)
+			
+			try:
+				print (next(res.results).text)
+				speak (next(res.results).text)
+			except StopIteration:
+				print ("No results")
+
+		# elif "" in query:
+			# Command go here
+			# For adding more commands
---
+++ README.md
@@ -0,0 +1,52 @@
+In the fast-paced world of technology, Artificial Intelligence has become an integral part of our 
+lives. From smartphones to smart homes, AI-powered devices are revolutionizing the way we 
+interact with technology. Today, we are thrilled to introduce Dora AI - your new virtual voice 
+assistant. 
+Dora AI is designed to be your trusted companion, always ready to assist with a wide range of 
+tasks and inquiries. Whether you need help setting reminders, answering questions, or even just 
+engaging in casual conversation, Dora AI is here to make your life easier and more enjoyable. 
+ 
+Operating in the domain of software web application, natural language processing, and artificial 
+intelligence, Dora AI is a technological marvel designed to revolutionize human-computer 
+interaction. 
+Dora AI is not just a voice assistant; it is an AI companion that offers a personalized, human-like 
+interaction. With a friendly conversational personality, Dora AI is more like an amiable ally, 
+always ready to assist, rather than a mundane machine. She communicates in a style that feels 
+warm, engaging, and relatable, fostering a relationship that transcends the boundaries of 
+traditional user-interface dynamics. 
+ 
+Dora AI's adaptability is one of its standout features. It tailors its tone and style of 
+communication based on various contexts and user preferences, ensuring a unique and 
+personalized user experience. It refines and evolves its interactions based on user feedback, 
+embodying a true user-centric design. 
+One of the key design principles of Dora AI is to make responses engaging. Beyond merely 
+executing tasks, Dora AI's replies are crafted to be enjoyable and enriching, adding a pleasant 
+layer to the interactions. Whether it's providing information or indulging in casual conversations, 
+Dora AI aims to make every interaction delightful. 
+ 
+At its core, Dora AI aims to humanize technology. It strives to create a more emotionally 
+resonant interaction, allowing users to experience technology in a more personal and user
+friendly way. By considering not just functionality but the emotional impact of interactions, Dora 
+AI is set to redefine the voice assistant landscape and create a stronger, more meaningful 
+connection between humans and technology.
+
+a) Hardware Requirements: 
+• Microphone: High-quality USB or Bluetooth with noise-canceling capabilities. 
+• Speaker: Clear and natural sound output. 
+• Processor: Multi-core with sufficient speed. 
+• Memory (RAM): Minimum 4GB for efficient processing. 
+• Storage: Adequate space for software, models, and resources. 
+• Internet Connectivity: Reliable connection for cloud-based services. 
+• Bluetooth/Wireless (Optional): If connecting to other devices wirelessly. 
+• Power Supply: Ensure a reliable power source, considering deployment scenarios. 
+ 
+(b) Software Requirements: 
+Operating System: Choose a compatible OS (Linux, Windows, macOS). 
+Development Environment: Use a suitable IDE (Visual Studio Code, PyCharm). 
+Programming Language: Python is common for voice assistant development. 
+Speech Recognition: Integrate a library/API (SpeechRecognition). 
+Natural Language Processing: Choose an NLP library/API (SpaCy). 
+Text-to-Speech: Integrate a library/API (pyttsx3, Google Text-to-Speech). 
+Version Control: Implement Git for code tracking and collaboration. 
+Frontend Development: HTML, CSS, JavaScript and Bootstrap 
+Internet Browser (Optional): Ensure compatibility with modern browsers.
